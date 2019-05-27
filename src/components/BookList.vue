1f1e95af76cf1da8e73efee4db2d82192d970c1a
Senay <ssahle28@gmail.com>
2019-05-27 17:14:55 -0500

    Looping throught list of books 1 through 5

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,15 +2,23 @@
 
 <div>
   <h1>{{title}}</h1>
+  <li v-for="book in books">{{book.title}} : {{book.author}}</li>
+
 </div>
 
 </template>
+
 <script>
 export default{
   name: "BookList",
   data(){
     return{
-      title: "All Books"
+      title: "All Books",
+      books: [
+    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+    {title: 'American Gods', author: 'Neil Gaiman'},
+    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+]
     }
   }
 }
@@ -20,6 +28,15 @@ export default{
 
   h1, h2{
     font-weight: normal
+  }
+
+  ul{
+    list-style-type: none;
+    padding: 0;
+  }
+  li{
+    display: block;
+    margin: 0 10px;
   }
 
 </style>
